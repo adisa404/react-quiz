fe72de0e5253d7a47f455d84f7e6fb7c1987798f
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-12-21 18:44:30 +0100

    add protocol file. add quiz.js.

---
+++ Protocol.md
@@ -0,0 +1,9 @@
+# init
+
+npm i
+
+npm run test
+
+# command pro module
+
+npm run test:module1
---
+++ src/Quiz.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+
+let quizData = import('./quiz_data.json');
