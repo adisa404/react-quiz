c0cb88eab900ae1e38470a26a19e4c91bde7c995
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-12-23 23:38:23 +0100

    add quiz question component. add quiz question button component.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion.js';
 
-let quizData = import('./quiz_data.json');
+let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { quiz_position: 1 };
   }
 
-  state = {};
   render() {
-    return <div>Quiz</div>;
+    return (
+      <div>
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
+      </div>
+    );
   }
 }
 
