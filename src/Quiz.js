ce85f71ab361bccbfaba9058e84499dc0967760d
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-12-23 20:51:50 +0100

    add quiz component. add constructor.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,16 @@
 import React, { Component } from 'react';
 
 let quizData = import('./quiz_data.json');
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {};
+  render() {
+    return <div>Quiz</div>;
+  }
+}
+
+export default Quiz;
